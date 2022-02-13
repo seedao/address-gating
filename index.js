0275c70cf9ee08d8fa71d8d6688ca44ef58a823a
TheCleopatra <cleopatracaesar8@gmail.com>
2022-02-13 16:50:34 +0800

    feat: add http fetch from Moralis and set cron job up for 10mins/call

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const url = require('url');
-const querystring = require('querystring');
-const cors=require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const url = require("url");
+const querystring = require("querystring");
+const cronJob = require("cron").CronJob;
+const cors = require("cors");
 
-// TODO: implement the code with MongoDB 
+// TODO: implement the code with MongoDB
 // const mongoose = require('mongoose');
 
 // load env config & init ENV varibles
-require('dotenv').config()
+require("dotenv").config();
 
-const MORALIS_KEY = process.env.MORALIS_KEY
-const PORT = process.env.PORT || 8080
-const CONTRACT_ADDR = process.env.CONTRACT_ADDR
+const MORALIS_KEY = process.env.MORALIS_KEY;
+const PORT = process.env.PORT || 8080;
+const CONTRACT_ADDR = process.env.CONTRACT_ADDR;
 
 // init express object
-const app = express()
+const app = express();
 
 // TODO: Figure it out how to use it
 // app.use(cors);
@@ -23,50 +25,88 @@ const app = express()
 // app.use(bodyParser.json());
 
 // init global variables
-// let owners = {}
-// TODO: fix this fixed value issue
-let owners = require('./data/owner').result;
+let owners = {};
 let ownerMap = new Map();
 
-app.get('/validate', (req, res) => {
-    let addr = req.query.addr;
-    // missing parameters
-    if(!addr) {
-        res.status(422);
-        res.send('None shall pass');
-    }
-    // switch to lower case
-    addr = addr.toLowerCase()
-    res.status(200);
-    if(ownerMap.has(addr)) {
-        res.json({ isOwner: true });
-    } else {
-        res.json({ isOwner: false });
-    }
-})
+app.get("/validate", (req, res) => {
+  let addr = req.query.addr;
+  // missing parameters
+  if (!addr) {
+    res.status(422);
+    res.send("None shall pass");
+  }
+  // switch to lower case
+  addr = addr.toLowerCase();
+  res.status(200);
+  if (ownerMap.has(addr)) {
+    res.json({ isOwner: true });
+  } else {
+    res.json({ isOwner: false });
+  }
+});
 
-app.get('/ping', (req, res) => {
-    console.log("ping")
-    res.send("PING")
-})
+app.get("/ping", (req, res) => {
+  console.log("ping");
+  res.send("PING");
+});
 
 function updateOwnerMap() {
-    for (let owner of owners) {
-        try {
-            ownerMap.set(owner.owner_of, true)
-        } catch(err) {
-            console.log(err)
-        }
+  ownerMap.clear();
+  for (let owner of owners) {
+    try {
+      ownerMap.set(owner.owner_of, true);
+    } catch (err) {
+      console.log(err);
     }
+  }
 }
 
-// TODO: 
-function cronFetchOwner() {
+async function fetchFromMoralis() {
+  const str = `https://deep-index.moralis.io/api/v2/nft/${CONTRACT_ADDR}/owners?chain=eth&format=decimal`;
+  // Fix: we have to put X-API-Key in Axios default header setting
+  // Refer to this article: https://stackoverflow.com/questions/60667416/put-api-key-in-axios
+  axios.defaults.headers.common = {
+    "X-API-Key": MORALIS_KEY,
+  };
+  const result = axios
+    .get(str, {
+      Headers: {
+        'accept': 'application/json',
+      }
+    })
+    .then((res) => {
+      return [res.data.result, null];
+    })
+    .catch((err) => {
+      console.error(err);
+      return [null, err];
+    });
+  return result;
+}
 
+function cronFetchOwner() {
+  // initialization of ownerMap
+  owners = require("./data/owner").result;
+  updateOwnerMap();
+
+  // fetch from the Moralis API periodically
+  // 10 mins per call
+  var fetchingJob = new cronJob(
+    "*/10 * * * *", 
+    async () => {
+    const ret = await fetchFromMoralis();
+      if(ret[1]) {
+        console.log('Errors when updating from Moralis')
+      } else {
+        owners = ret[0]
+        updateOwnerMap();
+        console.log('Update owner list successfully');
+      }
+    });
+  fetchingJob.start();
 }
 
 app.listen(PORT, () => {
-    console.log('[See DAO] address gating application online')
-    updateOwnerMap()
-})
-
+  console.log("[See DAO] address gating application online");
+  cronFetchOwner();
+});
